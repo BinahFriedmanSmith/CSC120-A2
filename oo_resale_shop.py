33bc5cfe2d15d22fc98ff1c5370032df42dd0a5e
Binah Friedman <bkfriedman@smith.edu>
2024-09-14 20:07:14 -0400

    did the assignment

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,101 @@
+from typing import Dict, Optional
+from computer import Computer
+
 class ResaleShop:
 
-    # What attributes will it need?
+    # Attributes
+    inventory: Dict[int, Computer] = {}
+    IDnum: int = 0
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # Constructor
+    def __init__(self):
+        self.inventory: Dict[int, Computer] = {}
+        self.IDnum: int = 0
+        
+    # Methods:
 
-    # What methods will you need?
+    def buy(self, computer: Computer):
+        #increment ID (prevent doubling up)
+        self.IDnum += 1
+        #add computer to inventory
+        self.inventory[self.IDnum] = computer
+        return self.IDnum
+        
+    #sell computer    
+    def sell(self, id: int):
+        #if inventory has computer w/ given ID
+        if id in self.inventory:
+            del self.inventory[id]
+            print("Item", id, "sold!")
+        else: 
+            print("Item", id, "not found. Please select another item to sell.")
+
+    #display inventory
+    def print_inventory(self):
+        # If the inventory is not empty
+        if self.inventory:
+            # For each item
+            for item_id in self.inventory:
+                # Print its details
+                print(f'Item ID: {item_id}, Description: {self.inventory[item_id].description}, Operating system: {self.inventory[item_id].operating_system}, Price: {self.inventory[item_id].price}')
+        else:
+            print("No inventory to display.")
+
+    #refurbish
+    def refurbish(self, item_id: int, new_os: Optional[str] = None):
+        if item_id in self.inventory:
+            computer = self.inventory[item_id] # locate the computer
+            if int(computer.year_made) < 2000:
+                computer.set_price(0) # too old to sell, donation only
+            elif int(computer.year_made) < 2012:
+                computer.set_price(250) # heavily-discounted price on machines 10+ years old
+            elif int(computer.year_made) < 2018:
+                computer.set_price(550) # discounted price on machines 4-to-10 year old machines
+            else:
+                computer.set_price(1000) # recent stuff
+
+            if new_os is not None:
+                computer.set_OS(new_os) # update details after installing new OS
+        else:
+            print("Item", item_id, "not found. Please select another item to refurbish.")
+
+
+def main():
+    #create the shop
+    store = ResaleShop()
+    #open for business!
+    print("-" * 21)
+    print("COMPUTER RESALE STORE")
+    print("-" * 21)
+
+    #buy a computer
+    id = store.buy(Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500))
+    print("Checking inventory...")
+    store.print_inventory()
+    print("Done.\n")
+
+    # refurbish it
+    new_OS = "MacOS Monterey"
+    print("Refurbishing Item ID:", id, ", updating OS to", new_OS)
+    print("Updating inventory...")
+    store.refurbish(id, new_OS)
+    print("Done.\n")
+
+    # Make sure it worked by checking inventory
+    print("Checking inventory...")
+    store.print_inventory()
+    print("Done.\n")
+    
+    # Now, let's sell it!
+    print("Selling Item ID:", id)
+    store.sell(id)
+    print("")
+    
+    # Make sure it worked by checking inventory
+    print("Checking inventory...")
+    store.print_inventory()
+    print("Done.\n")
+
+
+if __name__ == "__main__":
+    main()
--- reflection.md
+++ reflection.md
@@ -1,3 +1,5 @@
 Use this file to record your reflection on this assignment. 
 
 What worked, what didn't, what advice would you give someone taking this course in the future?
+
+The most difficult part for me was just adjusting to the requirements of Python syntax, since I had only done OOP in other languages. I had one minor error where i put main inside the computer class, and one where i didn't put attributes in the constructor. Overall I didn't really struggle with this one though. My best advice is to comment as you go, so you don't have to go back and add documentation at the end. Doing that is always either confusing or very boring.
